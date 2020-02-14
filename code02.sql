2fe8923cd06d553695da983ac4a94ced5bb7dcf9
Sevdanur GENC <sevdanurgenc@gmail.com>
2020-02-14 22:04:59 +0300

    Create code02.sql

---
+++ code02.sql
@@ -0,0 +1,9 @@
+--tabloya veri eklemek
+insert into musteri(id,ad,soyad,sehir) values(6,'Sevdanur','Genc','Isparta');
+insert into musteri(id,ad,soyad,sehir) values(7,'Selcuk','Genc','USA');
+ 
+insert into musteri(id,ad,soyad,sehir) values
+(8,'birsen','durusoy','balikesir'),
+(9,'abc','def','istanbul');
+
+select * from musteri

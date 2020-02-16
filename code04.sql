f12efcc2b977c2e8e7edd514e0dd973915f5569a
Sevdanur GENC <sevdanurgenc@gmail.com>
2020-02-16 14:47:15 +0300

    Create code04.sql

---
+++ code04.sql
@@ -0,0 +1,11 @@
+-- delete kullanimi
+delete from musteri where id=10
+select * from musteri
+
+-- update kullanimi
+update musteri set bakiye = 5750 where id = 1
+select * from musteri
+-- select * from musteri order by id
+
+update musteri set sehir = 'Kastamonu' where sehir = 'Ankara'
+select * from musteri order by id

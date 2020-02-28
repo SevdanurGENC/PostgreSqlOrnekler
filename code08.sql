756149e6f4e5678d7ecb6fac0cd59d6fd0bdf180
Sevdanur GENC <sevdanurgenc@gmail.com>
2020-02-28 18:36:26 +0300

    Update code08.sql

--- code08.sql
+++ code08.sql
@@ -8,6 +8,17 @@ select ogrno, AVG(ortalama)
 from  public."tblOgrenci" 
 group by ogrno
 
+-- having ifadesi
+SELECT fakulte, AVG(ortalama) 
+FROM public."tblOgrenci"
+GROUP BY fakulte 
+HAVING AVG(ortalama) > 2.5
+ 
+SELECT fakulte, COUNT(*) 
+FROM public."tblOgrenci"
+GROUP BY fakulte 
+HAVING COUNT(*) > 1
+
 -- ortalama alma fonksiyonu
 select AVG(ortalama)
 from public."tblOgrenci"  
